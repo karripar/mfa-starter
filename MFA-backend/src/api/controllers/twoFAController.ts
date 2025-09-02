2f0e6ade55c34723bcc0d18319379bb90a857b86
karripar <karripartanen25@gmail.com>
2025-09-02 11:30:01 +0300

    fixed

--- MFA-backend/src/api/controllers/twoFAController.ts
+++ MFA-backend/src/api/controllers/twoFAController.ts
@@ -1,14 +1,47 @@
 import CustomError from '../../classes/CustomError';
+import {Request, Response, NextFunction} from 'express';
+import {User} from '@sharedTypes/DBTypes';
+import {UserResponse} from '@sharedTypes/MessageTypes';
+import fetchData from '../../utils/fetchData';
+import OTPAuth from 'otpauth';
+
 // TODO: Import necessary types and models
 
 // TODO: Define setupTwoFA function
-const setupTwoFA = async (req, res, next) => {
+const setupTwoFA = async (
+  req: Request<{}, {}, User>,
+  res: Response<{qrCodeUrl: string}>,
+  next: NextFunction,
+) => {
   try {
     // TODO: Register user to AUTH API
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    };
 
+    const userResponse = await fetchData<UserResponse>(
+      `${process.env.AUTH_URL}/api/v1/users`,
+      options,
+    )
+
+    console.log(userResponse);
     // TODO: Generate a new 2FA secret
+    const secret = new OTPAuth.Secret();
 
     // TODO: Create the TOTP instance
+    const totp = new OTPAuth.TOTP({
+      issuer: 'ElukkaAPI',
+      label: userResponse.user.email,
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret: secret,
+    });
+
 
     // TODO: Store or update the 2FA data in the database
 
@@ -19,18 +52,19 @@ const setupTwoFA = async (req, res, next) => {
 };
 
 // TODO: Define verifyTwoFA function
-const verifyTwoFA = async (req, res, next) => {
+const verifyTwoFA = async (
+  req: Request<{}, {}, {email: string; code: string}>,
+  res: Response,
+  next: NextFunction,
+) => {
   const {email, code} = req.body;
+  console.log(email, code);
 
   try {
     // TODO: Retrieve 2FA data from the database
-
     // TODO: Validate the 2FA code
-
     // TODO: If valid, get the user from AUTH API
-
     // TODO: Create and return a JWT token
-
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
--- MFA-backend/src/api/models/twoFAModel.ts
+++ MFA-backend/src/api/models/twoFAModel.ts
@@ -1,11 +1,16 @@
 import {model, Schema} from 'mongoose';
 import {TwoFA} from '../../types/2FA';
 
+
 const TwoFASchema = new Schema<TwoFA>({
   // TODO: add userId (Number, required, unique)
+  userId: {type: Number, required: true, unique: true},
   // TODO: add email (String, required, unique)
+  email: {type: String, required: true, unique: true},
   // TODO: add twoFactorSecret (String, required)
+  twoFactorSecret: {type: String, required: true},
   // TODO: add twoFactorEnabled (Boolean, default: false)
+  twoFactorEnabled: {type: Boolean, default: false},
 });
 
 export default model<TwoFA>('TwoFA', TwoFASchema);
